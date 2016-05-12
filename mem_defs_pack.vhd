c8f9004b82f7a27b0b1f0eba6eab6a8baaa3f315
rchaaben <chaabenramzi@gmail.com>
2016-05-12 10:58:10 +0200

    Add files via upload

--- mem_defs_pack.vhd
+++ mem_defs_pack.vhd
@@ -1,18 +1,23 @@
 use WORK.cpu_defs_pack.all;
 
-package mem_defs_pack is
-	function init_memory return mem_type;
+
+package mem_defs_pack is 
+  function init_memory return mem_type;
+  constant memory_content : mem_type;
 end mem_defs_pack;
 
 package body mem_defs_pack is
-	function init_memory return mem_type is
-	begin
-
-	return(
-	0 => code_nop * (2**reg_addr_width)**3,
-	1 => code_stop * (2**reg_addr_width)**3,
-	others => 0 
-	);
-
-	end init_memory;
+  --- function init_memory 
+  function init_memory return mem_type is 
+  begin 
+    return 
+          (0   => code_nop * (2**reg_addr_width) **3,
+           1   => code_stop * (2**reg_addr_width) **3,
+           others => 0);
+  end init_memory;
+  --- Constant memory_content
+  constant memory_content : mem_type :=
+          (0   => code_nop * (2**reg_addr_width) **3,
+           1   => code_stop * (2**reg_addr_width) **3,
+           others => 0);
 end mem_defs_pack;
